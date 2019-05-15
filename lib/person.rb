d15cef527d9a137895bcccfc2e994dbe047bb90c
fortunate-jest-6040 <pedro.loureiro7@hotmail.com>
2019-05-15 09:01:21 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  def initialize(name,job)
+    @name = name
+    @job = job
+  end
+  
+  def get_name
+    @name
+  end
+  
+  def set_name=(name)
+    @name = name
+  end
+  
+  def get_job
+    @job
+  end
+  
+  def set_job(job)
+    @job = job
+  end
+end
